dc99e7b9af71d0bc8c7975d1e235949f19c77f21
Tliss1984 <tliss1984@aim.com>
2020-10-15 19:54:07 -0400

    Create exericse 5.1.py

---
+++ exericse 5.1.py
@@ -0,0 +1,21 @@
+
+
+
+
+import time
+
+
+print ("Days since epoch:", str(time.time()/(60*60*24)),
+       "\nHours since epoch:", str(time.time()/3600),
+       "\nMinutes since epoch:", str(time.time()/ 60),
+       "\nSeconds since epoch:", str(time.time()))
+
+
+
+    
+
+
+
+
+
+
