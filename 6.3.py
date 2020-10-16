6a7d7cd98c11496ef8a7d687832e55bcc1c57192
Tliss1984 <tliss1984@aim.com>
2020-10-16 12:44:52 -0400

    Create 6.3.py

---
+++ 6.3.py
@@ -0,0 +1,14 @@
+
+
+
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+    if word[0] != word[-1]:
+        return False
+    return is_palindrome(word[1:-1])
+
+
+print(is_palindrome('Taurean Liss'))
+print(is_palindrome('Taurean LissssiL naeruaT'))
+
